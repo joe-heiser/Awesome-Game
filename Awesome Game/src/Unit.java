14cfe23ba4d6a9e54a6a81d86e5e7054c0782771
LukeDesktop <lcetnar123@gmail.com>
2017-12-23 01:47:35 -0500

    a

--- Awesome Game/src/Unit.java
+++ Awesome Game/src/Unit.java
@@ -1,75 +1,99 @@
 
 public class Unit {
+
+	// Start Declare Attributes
+	private String Name;
+	private int Health;
+	private int Coin;
+	private int Attackval;
+	// End Declare Attributes
+
+	// Start Declare Positional Data
+	private int Ypos;
+	private int Xpos;
+	private int Yvel;
+	private int Xvel;
+	// End Declare Positional Data
+
+	// Start Getters and Setters for Positional
+	public int getYpos() {
+		return Ypos;
+	}
+
+	public void setYpos(int ypos) {
+		Ypos = ypos;
+	}
+
+	public int getXpos() {
+		return Xpos;
+	}
+
+	public void setXpos(int xpos) {
+		Xpos = xpos;
+	}
+
+	public int getYvel() {
+		return Yvel;
+	}
+
+	public void setYvel(int yvel) {
+		Yvel = yvel;
+	}
+
+	public int getXvel() {
+		return Xvel;
+	}
+
+	public void setXvel(int xvel) {
+		Xvel = xvel;
+	}
+	// End Getters and Setters for Positional
+
+	// Start Getters for Attributes
+	public String getName() {
+		return Name;
+	}
+
+	public int getHealth() {
+		return Health;
+	}
+
+	public int getCoin() {
+		return Coin;
+	}
+	// End Getters for Attributes
+
+	// Start Constructors
+	public Unit(String name, int health, int coin) {
+		super();
+		Name = name;
+		Health = health;
+		Coin = coin;
+	}
+
+	public Unit(String name) {
+		super();
+		Name = name;
+		Health = 100;
+		Coin = 100;
+	}
+	// End Constructors
+
+	// Actions
+	public void Heal() {
+		if (this.Health > 0) {
+			this.Health += 10;
+			if (this.Health > 100) {
+				this.Health = 100;
+			}
+		} else {
+		}
+
+	}
+	public void attack(Unit u) {
+		
+	}
 	
-//Start Declare Attributes
-private String Name;
-private int Health;
-private int Coin;
-//End Declare Attributes
-
-//Start Declare Positional Data
-private int Ypos;
-private int Xpos;
-private int Yvel;
-private int Xvel;
-//End Declare Positional Data
-
-
-//Start Getters and Setters for Positional
-public int getYpos() {
-	return Ypos;
-}
-public void setYpos(int ypos) {
-	Ypos = ypos;
-}
-public int getXpos() {
-	return Xpos;
-}
-public void setXpos(int xpos) {
-	Xpos = xpos;
-}
-public int getYvel() {
-	return Yvel;
-}
-public void setYvel(int yvel) {
-	Yvel = yvel;
-}
-public int getXvel() {
-	return Xvel;
-}
-public void setXvel(int xvel) {
-	Xvel = xvel;
-}
-//End Getters and Setters for Positional
-
-//Start Getters for Attributes
-public String getName() {
-	return Name;
-}
-public int getHealth() {
-	return Health;
-}
-public int getCoin() {
-	return Coin;
-}
-//End Getters for Attributes
-
-
-
-//Start Constructors
-public Unit(String name, int health, int coin) {
-	super();
-	Name = name;
-	Health = health;
-	Coin = coin;
-}
-
-public Unit(String name) {
-	super();
-	Name = name;
-	Health = 100;
-	Coin = 100;
-}
-//End Constructors
 
 }
+
