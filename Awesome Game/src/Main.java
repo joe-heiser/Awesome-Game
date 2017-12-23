e9d3d2390eec9bef100df130fe1c30c1b058ed78
joe-heiser <34702912+joe-heiser@users.noreply.github.com>
2017-12-23 01:23:54 -0500

    First Commit

---
+++ Awesome Game/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	/**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private Main() {
+    }
+    
+    public static void main(String[] args) {
+    
+    	System.out.println("Niggers");
+    	
+    }
+
+}
